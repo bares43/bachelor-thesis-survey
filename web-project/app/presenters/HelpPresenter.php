04cbbd2019186beaf861f5d2a3a46df4c93df8d5
Jan Bares <janbares43@gmail.com>
2015-08-26 12:00:02 +0200

    homepage

---
+++ web-project/app/presenters/HelpPresenter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janba_000
+ * Date: 26. 8. 2015
+ * Time: 10:28
+ */
+namespace App\Presenters;
+
+use Nette;
+use App\Model;
+
+class HelpPresenter extends Nette\Application\UI\Presenter {
+
+
+}
---
+++ web-project/app/presenters/WhatIsWireframePresenter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janba_000
+ * Date: 26. 8. 2015
+ * Time: 9:50
+ */
+namespace App\Presenters;
+
+use Nette;
+use App\Model;
+
+class WhatIsWireframePresenter extends Nette\Application\UI\Presenter {
+
+
+}
